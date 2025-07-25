69e4670df4648dccd9ccf2f9a731411b5b9a4c40
chinajaxon <njokuchina@gmail.com>
2025-07-25 13:34:31 +0100

    Add service worker registration

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,13 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ServiceWorkerRegister from "../components/ServiceWorkerRegister";
 
 export const metadata = {
   title: "AI Seminar & Thesis Generator",
   description: "Generate seminar and thesis content easily",
-   icons: {
-    icon: "/images/logo.png", // or /favicon.ico
+  icons: {
+    icon: "/images/logo.png",
   },
 };
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="bg-black text-white">
         <Navbar />
+        <ServiceWorkerRegister />
         {children}
         <Footer />
       </body>
